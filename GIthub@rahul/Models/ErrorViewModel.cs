e3a18f0108ad4c41bf5a031586d9ca043a07e04d
Vaibhav Kanna <kannavaibhavgupta@gmail.com>
2025-08-08 23:27:06 +0530

    Add project files.

---
+++ GIthub@rahul/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GIthub_rahul.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
